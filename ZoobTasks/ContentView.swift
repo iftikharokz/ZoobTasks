17643d97afd47c9aaf390357c3a44b385823d1f9
TheAppMedia <iftikharhussain2230@gmail.com>
2023-02-15 14:23:44 +0500

    Initial Commit

---
+++ ZoobTasks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZoobTasks
+//
+//  Created by Theappmedia on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZoobTasks/ZoobTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoobTasksApp.swift
+//  ZoobTasks
+//
+//  Created by Theappmedia on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct ZoobTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
